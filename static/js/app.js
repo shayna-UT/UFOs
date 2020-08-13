cd5f9c37027a8481edb8e79a73d641c5f8d9fbe5
Shayna Sims <shaynarsims@yahoo.com>
2020-08-13 16:35:27 -0500

    create dynamic table that can filter by date

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,10 @@
-// import the data from data.js
+// Import the data from data.js
 const tableData = data;
 
-// reference the HTML table using d3
+// Reference the HTML table using d3
 var tbody = d3.select("tbody");
 
-// build a table
+// Create a function to build a table
 function buildTable(data) {
     // First, clear out any existing data
     tbody.html("");
@@ -24,3 +24,29 @@ function buildTable(data) {
         );
     });
 }
+
+// Add the ability to filter the data in the table
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    // Check to see if a date was entered 
+    // and filter the data using that date
+    if (date) {
+        // Apply `filter` to the table data to only keep the rows
+        // where the `datetime` value matches the filter value 
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+
+    // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, 
+    // then filteredData will just be the original tableData
+    buildTable(filteredData);
+}
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
